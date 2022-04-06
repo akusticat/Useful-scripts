56d0228b2302955f8d58b13f367d11f230b78021
akusticat <37180810+akusticat@users.noreply.github.com>
2022-04-06 09:59:46 +0300

    Add files via upload
    Who are the sign in user  in which computer

---
+++ compoter & user.ps1
@@ -0,0 +1,13 @@
+﻿$time=[datetime]::Today.AddDays(-90)
+$ADcomputers = Get-ADComputer -Filter {LastLogonDate -gt $time } -prop * #|select name,LastLogonDate |ft -auto
+
+    foreach ($computer in $ADcomputers) {     
+         
+         $output = @{ 'ComputerName' = $comp }
+         $output.UserName = (Get-WmiObject -Class win32_computersystem -ComputerName $computer.name).UserName
+         [PSCustomObject]$output
+         }
+
+  
+  $ADcomputers |select name,LastLogonDate | Export-Csv -Path c:\LastLogon.csv  
+ 
